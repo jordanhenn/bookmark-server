bc2b1f5faf8cac57561569a1b08d0233dd2bf656
Jordan Henn <jordanwalkerhenn@gmail.com>
2020-08-14 00:18:39 -0700

    refactored post and delete endpoints to use knex database and sanitize XSS attacks

--- src/bookmark/bookmarkRouter.js
+++ src/bookmark/bookmarkRouter.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const xss = require('xss')
 const { v4: uuid } = require('uuid')
 const { bookmarks } = require('../store')
 const BookmarksService = require('../bookmarks-service')
@@ -6,93 +7,107 @@ const BookmarksService = require('../bookmarks-service')
 const bookmarkRouter = express.Router()
 const bodyParser = express.json()
 
+const serializeBookmark = bookmark => ({
+  id: bookmark.id,
+  title: xss(bookmark.title),
+  url: xss(bookmark.url),
+  description: xss(bookmark.description),
+  rating: bookmark.rating
+})
+
 bookmarkRouter
   .route('/bookmarks')
   .get((req, res, next) => {
     const knexInstance = req.app.get('db')
     BookmarksService.getAllBookmarks(knexInstance)
       .then(bookmarks => {
-        res.json(bookmarks)
+        res.json(bookmarks.map(serializeBookmark))
       })
       .catch(next)
   })
-  .post(bodyParser, (req, res) => {
-    const { title, url, rating, desc } = req.body;
-
+  .post(bodyParser, (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    const { title, url, rating, description } = req.body;
+    const newBookmark = { title, url, rating, description }
     if (!title) {
-        return res
-            .status(400)
-            .send('Invalid data');
+      return res.status(400).json({
+        error: { message: `Missing 'title' in request body`}
+      })
     }
 
     if (!url) {
-        return res
-            .status(400)
-            .send('Invalid data');
+      return res.status(400).json({
+        error: { message: `Missing 'url' in request body`}
+      })
     }
 
-    if (!rating) {
-        return res
-            .status(400)
-            .send('Invalid data');
+    if (!rating || rating < 1 || rating > 5) {
+      return res.status(400).json({
+        error: { message: `Missing a rating between 1 and 5`}
+      })
     }
 
-    if (!desc) {
-        return res
-            .status(400)
-            .send('Invalid data');
-    }
-
-    const id = uuid();
-
-    const bookmark = {
-        id,
-        title,
-        url,
-        rating,
-        desc
-    };
-
-    bookmarks.push(bookmark);
-
-    res
-    .status(201)
-    .location(`http://localhost:8000/bookmarks/${id}`)
-    .json({bookmark});
+    for (const [key, value] of Object.entries(newBookmark)) {
+          newBookmark[key] = xss(value)
+  }
+    BookmarksService.insertBookmark(
+      knexInstance,
+      newBookmark
+    )
+    .then(bookmark => {
+      res
+      .status(201)
+      .location(`/bookmarks/${bookmark.id}`)
+      .json(bookmark)
+    })
+    .catch(next)
   })
 
 bookmarkRouter
   .route('/bookmarks/:id')
+  .all((req, res, next) => {
+    BookmarksService.getById(
+        req.app.get('db'),
+        req.params.id
+    )
+    .then(bookmark => {
+        if (!bookmark) {
+            return res.status(404).json({
+                error: { message: `Bookmark doesn't exist`}
+            })
+        }
+        res.bookmark = bookmark
+        next()
+    })
+    .catch(next)
+})
   .get((req, res, next) => {
     const knexInstance = req.app.get('db')
     const { id } = req.params;
     BookmarksService.getById(knexInstance, id)
       .then(bookmark => {
-        if (!bookmark) {
-          return res
-              .status(404)
-              .send({ error: { message: 'Bookmark Not Found' }});
-        }
-        res.json(bookmark)
+        res.json({
+          id: res.bookmark.id,
+          url: xss(res.bookmark.url),
+          title: xss(res.bookmark.title),
+          description: xss(res.bookmark.description),
+          rating: (res.bookmark.rating)
+      })
+  })
+})
+  .delete((req, res, next) => {
+    const { id } = req.params
+    const knexInstance = req.app.get('db')
+    BookmarksService.deleteBookmark(
+      knexInstance,
+      id
+    )
+      .then(() => {
+        res
+        .status(204)
+        .end()
       })
       .catch(next)
-  })
-  .delete((req, res) => {
-    const { id } = req.params;
-
-    const bookmarkIndex = bookmarks.findIndex(bm => bm.id === id);
-
-    if (bookmarkIndex === -1) {
-        return res
-            .status(404)
-            .send('Not found');
-    }
-
-    bookmarks.splice(bookmarkIndex, 1);
-
-    res
-        .status(204)
-        .end();
   })
 
 module.exports = bookmarkRouter
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -40,22 +40,208 @@ describe('Bookmarks Endpoints', function() {
                 .insert(testBookmarks)
             })
       
-            it('responds with 200 and all of the articles', () => {
+            it('responds with 200 and all of the bookmarks', () => {
               return supertest(app)
                 .get('/bookmarks')
                 .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
                 .expect(200, testBookmarks)
             })
           })
+          context(`Given an XSS attack article`, () => {
+            const testBookmarks = [
+              {
+                id: 911,
+                title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+                url: 'https://www.goofballboys.com',
+                description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+                rating: 2
+              },
+              {
+                id: 2,
+                title: 'Goofball Gals',
+                url: 'https://www.goofballgals.com',
+                description: 'A wesbite for the goofball gals.',
+                rating: 3
+              },
+              {
+                id: 3,
+                title: 'Reggie Jackson',
+                url: 'https://www.reggiejackson.com',
+                description: `Reggie Jackson's personal website.`,
+                rating: 5
+              }
+          ]
+          
+          const sanitizedBookmarks = [
+            {
+              id: 911,
+              title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+              url: 'https://www.goofballboys.com',
+              description: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
+              rating: 2
+            },
+            {
+              id: 2,
+              title: 'Goofball Gals',
+              url: 'https://www.goofballgals.com',
+              description: 'A wesbite for the goofball gals.',
+              rating: 3
+            },
+            {
+              id: 3,
+              title: 'Reggie Jackson',
+              url: 'https://www.reggiejackson.com',
+              description: `Reggie Jackson's personal website.`,
+              rating: 5
+            }
+          ]
+    
+            beforeEach('insert malicious article', () => {
+              return db
+                .into('bookmarks')
+                .insert(testBookmarks)
+            })
+    
+            it('removes XSS attack content', () => {
+              return supertest(app)
+                .get(`/bookmarks`)
+                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                .expect(200, sanitizedBookmarks)
+            })
+        })    
     })
+
+    describe(`POST /bookmarks`, () => {
+      it(`creates a bookmark, responding with 201 and the new bookmark`, function() {
+        this.retries(3)
+        const testBookmarks = makeBookmarksArray()
+        const testBookmark = testBookmarks[0]
+        return supertest(app)
+          .post('/bookmarks')
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .send(testBookmark)
+          .expect(201)
+          .expect(res => {
+            expect(res.body.title).to.eql(testBookmark.title)
+            expect(res.body.url).to.eql(testBookmark.url)
+            expect(res.body.description).to.eql(testBookmark.description)
+            expect(res.body.rating).to.eql(testBookmark.rating)
+            expect(res.body).to.have.property('id')
+            expect(res.headers.location).to.eql(`/bookmarks/${res.body.id}`)
+          })
+          .then(postRes => 
+              supertest(app)
+                .get(`/bookmarks/${postRes.body.id}`)
+                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                .expect(postRes.body)
+          )
+      })
+
+      it(`sanitizes posted article containing XSS attack`, function() {
+        this.retries(3)
+        const maliciousBookmark = {
+          id: 911,
+          title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+          url: 'https://www.google.com',
+          description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+          rating: 3
+        }
+        return supertest(app)
+          .post('/bookmarks')
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .send(maliciousBookmark)
+          .expect(201)
+          .expect(res => {
+            expect(res.body.title).to.eql('Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;')
+            expect(res.body.url).to.eql(maliciousBookmark.url)
+            expect(res.body.description).to.eql(`Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`)
+            expect(res.body.rating).to.eql(maliciousBookmark.rating)
+            expect(res.body).to.have.property('id')
+            expect(res.headers.location).to.eql(`/bookmarks/${res.body.id}`)
+          })
+          .then(postRes => 
+              supertest(app)
+                .get(`/bookmarks/${postRes.body.id}`)
+                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                .expect(postRes.body)
+          )
+      })
+    
+
+        it(`responds with 400 and an error message where the 'title' is missing`, () => {
+          const newBookmark = {
+            url: 'https://www.newtestbookmark.com',
+            rating: 3
+          }
+
+          return supertest(app)
+            .post('/bookmarks')
+            .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+            .send(newBookmark)
+            .expect(400)
+            .expect({
+              error: { message: `Missing 'title' in request body` }
+            })
+        })
+
+        it(`responds with 400 and an error message where the 'url' is missing`, () => {
+          const newBookmark = {
+            title: 'Test new bookmark',
+            rating: 3
+          }
+
+          return supertest(app)
+            .post('/bookmarks')
+            .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+            .send(newBookmark)
+            .expect(400)
+            .expect({
+              error: { message: `Missing 'url' in request body` }
+            })
+        })
+
+        it(`responds with 400 and an error message where the 'rating' is missing`, () => {
+          const newBookmark = {
+            title: 'Test new bookmark',
+            url: 'https://www.newtestbookmark.com'
+          }
+
+          return supertest(app)
+            .post('/bookmarks')
+            .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+            .send(newBookmark)
+            .expect(400)
+            .expect({
+              error: { message: `Missing a rating between 1 and 5` }
+            })
+        })
+
+
+
+        it(`responds with 400 and an error message where rating is not between 1 and 5`, () => {
+          const newBookmark = {
+            title: 'Test new bookmark',
+            url: 'https://www.newtestbookmark.com',
+            rating: 20
+          }
+          return supertest(app)
+            .post('/bookmarks')
+            .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+            .send(newBookmark)
+            .expect(400, {
+              error: { message: `Missing a rating between 1 and 5` }
+            })
+        })
+      })
+
     describe(`GET /bookmarks/:id`, () => {
-        context('Given no articles', () => {
+        context('Given no bookmarks', () => {
             it('responds with 404', () => {
                 const bookmarkId = 123456
                 return supertest(app)
                   .get(`/bookmarks/${bookmarkId}`)
                   .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-                  .expect(404, { error: { message: 'Bookmark Not Found' }})
+                  .expect(404, { error: { message: `Bookmark doesn't exist` }})
             })
         })
         context('Given there are bookmarks in the database', () => {
@@ -76,5 +262,65 @@ describe('Bookmarks Endpoints', function() {
               .expect(200, expectedBookmark)
           })
         })
+
+        context(`Given an XSS attack article`, () => {
+          const maliciousBookmark = {
+            id: 911,
+            title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+            url: 'https://www.goofballboys.com',
+            description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+            rating: 2
+          }
+          beforeEach('insert malicious bookmark', () => {
+            return db
+              .into('bookmarks')
+              .insert([ maliciousBookmark ])
+          })
+
+          it('removes XSS attack content', () => {
+            return supertest(app)
+              .get(`/bookmarks/${maliciousBookmark.id}`)
+              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+              .expect(200)
+              .expect(res => {
+                expect(res.body.title).to.eql('Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;')
+                expect(res.body.description).to.eql(`Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`)
+              })
+          })
       })
+  })
+  describe(`Delete /bookmarks/:bookmark_id`, () => {
+    context('Given there are articles in the database', () => {
+      const testBookmarks = makeBookmarksArray()
+
+      beforeEach('insert bookmarks', () => {
+        return db
+          .into('bookmarks')
+          .insert(testBookmarks)
+      })
+      it('responds with 204 and removes the article', () => {
+        const idToRemove = 2
+        const expectedBookmarks = testBookmarks.filter(bookmark => bookmark.id !== idToRemove)
+        return supertest(app)
+          .delete(`/bookmarks/${idToRemove}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get(`/bookmarks`)
+              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+              .expect(expectedBookmarks)
+          )
+      })
+    })
+    context(`Given no articles`, () => {
+      it(`responds with 404`, () => {
+        const bookmarkId = 123456
+        return supertest(app)
+          .delete(`/bookmarks/${bookmarkId}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(404, { error: { message: `Bookmark doesn't exist`} })
+      })
+    })
+  })
 })
